31fcc4208c30846e2c4c2a784a22ce2ebd41c5fa
Alstudd <alstonsoares17@gmail.com>
2025-01-09 11:31:53 +0530

    dotenv for port

--- stocksStats/index.js
+++ stocksStats/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 
 const { extractStocks } = require("./controller/extractStocks");
+
+require("dotenv").config();
+
 const app = express();
 const cors = require("cors");
+
+const port = process.env.PORT || 3001;
 
 // Custom CORS configuration example
 const corsOptions = {
@@ -16,8 +21,8 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 app.get("/stocks", async (req, res) => {
